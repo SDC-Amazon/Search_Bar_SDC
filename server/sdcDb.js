021e1848ed78f34f558c0409527702ce6e90be68
Tye <tyemacon@gmail.com>
2019-10-31 15:04:02 -0500

    bar can retrive product by id (0-9999999)

--- server/sdcDb.js
+++ server/sdcDb.js
@@ -3,7 +3,7 @@ const { Client } = require('pg')
 const client = new Client({
   host: 'localhost',
   port: 5432,
-  user: 'me',
+  user: 'postgres',
   password: 'password',
   database: 'sdc'
 })
@@ -12,12 +12,23 @@ client.connect()
   .then(() => console.log('database connected!'));
 
 const getAllProducts = async (callback) => {
+  console.log('querying ALL db...')
   let query = `SELECT * FROM products;`;
   await client
     .query(query)
     .then((results) => callback(null, results))
     .catch((err) => callback(err, null))
-  
 }
 
+const getProduct = async (searchString, callback) => {
+  console.log(`querying for product ${searchString}...`);
+  let query = `SELECT * FROM products WHERE id = ${searchString}`;
+  await client
+    .query(query)
+    .then((results) => callback(null, results))
+    .catch((err) => callback(err, null))
+}
+
+
 module.exports.getAllProducts = getAllProducts;
+module.exports.getProduct = getProduct;
--- server/sdcServer.js
+++ server/sdcServer.js
@@ -1,9 +1,11 @@
-const { getAllProducts } = require('./sdcDb');
+const { getAllProducts, getProduct } = require('./sdcDb');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname + '/../dist/')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -12,6 +14,17 @@ app.get('/all', (req, res) => {
     if(err){
       throw err;
     }else{
+      res.send(results);
+    }
+  })
+})
+
+app.get('/search/:searchString', (req, res) => {
+  getProduct(req.params.searchString, (err, results) => {
+    if(err){
+      throw err
+    }else{
+      console.log(results.rows);
       res.send(results);
     }
   })
